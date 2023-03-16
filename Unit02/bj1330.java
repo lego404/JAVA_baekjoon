78368cc3c26707ef3b1ee258d3992bb8dadd2840
Gone <71643183+lego404@users.noreply.github.com>
2023-03-16 16:19:59 +0900

    조건문

---
+++ Unit02/bj1330.java
@@ -0,0 +1,15 @@
+package Unit02;
+//import java.io.*;
+import java.util.Scanner;
+public class bj1330 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A, B;
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if(A > B) System.out.println(">");
+		else if(A < B) System.out.println("<");
+		else if(A == B) System.out.println("==");
+	}
+}
