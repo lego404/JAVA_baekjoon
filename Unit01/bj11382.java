11a914e0acbf18823b06cd7165ea0b633f00eb12
Gone <71643183+lego404@users.noreply.github.com>
2023-03-16 15:43:56 +0900

    꼬마정민

---
+++ Unit01/bj11382.java
@@ -0,0 +1,12 @@
+package Unit01;
+//import java.io.*;
+import java.util.Scanner;
+public class bj11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		System.out.println(A + B + C);
+	}
+}
