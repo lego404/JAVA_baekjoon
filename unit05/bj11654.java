5ca7e1f562f721017eb3fb5bcd2cedfed4721988
Gone <71643183+lego404@users.noreply.github.com>
2023-05-03 02:03:30 +0900

    아스키코드

---
+++ unit05/bj11654.java
@@ -0,0 +1,11 @@
+package unit05;
+//import java.io.*;
+import java.util.*;
+public class bj11654 {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int input = (int)sc.nextLine().charAt(0);	//문자 입력받아서 정수형에 저장 
+		System.out.println(input);
+		
+	}
+}
