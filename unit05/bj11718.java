3e6b542988445c2132db48305fde7192d2ac112b
Gone <71643183+lego404@users.noreply.github.com>
2023-06-23 03:48:41 +0900

    그대로 출력하기

---
+++ unit05/bj11718.java
@@ -0,0 +1,16 @@
+package unit05;
+//import java.io.*;
+import java.util.*;
+public class bj11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] str = new String[100];
+
+		while(sc.hasNext()) {
+			int i = 0;
+			str[i] = sc.nextLine();
+			System.out.println(str[i]);
+			i++;
+		}
+	}
+}
